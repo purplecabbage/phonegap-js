12b040324d40ba16c396dff2e30057fd81348e72
Michael Brooks <michael@michaelbrooks.ca>
2011-07-05 17:25:02 -0700

    Fix parse error with Android source.

--- src/android/media.js
+++ src/android/media.js
@@ -14,10 +14,10 @@
 PG.MediaError = {
     code: null,
     message: "",
-    MEDIA_ERR_ABORTED                   : 1,
-    MediaError.MEDIA_ERR_NETWORK        : 2,
-    MediaError.MEDIA_ERR_DECODE         : 3,
-    MediaError.MEDIA_ERR_NONE_SUPPORTED : 4
+    MEDIA_ERR_ABORTED        : 1,
+    MEDIA_ERR_NETWORK        : 2,
+    MEDIA_ERR_DECODE         : 3,
+    MEDIA_ERR_NONE_SUPPORTED : 4
 };
 
 /**
