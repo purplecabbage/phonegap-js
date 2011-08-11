6f28bf730a746eb26a0a041239a629fc671d35fb
Don Coleman <dcoleman@chariotsolutions.com>
2011-08-10 22:59:01 -0400

    add shim to adjust iOS javascript

--- src/notification.js
+++ src/notification.js
@@ -27,7 +27,6 @@ navigator.notification = {
     alert: function(message, completeCallback, title, buttonLabel) {
         var _title = (title || "Alert");
         var _buttonLabel = (buttonLabel || "OK");
-        // TODO iOS has an object for title and buttonLabel
         PhoneGap.exec(completeCallback, null, "Notification", "alert", [message, _title, _buttonLabel]);
     },
 
@@ -43,62 +42,8 @@ navigator.notification = {
     confirm: function(message, resultCallback, title, buttonLabels) {
         var _title = (title || "Confirm");
         var _buttonLabels = (buttonLabels || "OK,Cancel");
-        // TODO iOS calls alert() - PGNotification should implement confirm
         PhoneGap.exec(resultCallback, null, "Notification", "confirm", [message, _title, _buttonLabels]);
     },
-
-    // Deprecated - Removed from 1.0 according to iOS
-    // /**
-    //  * Start spinning the activity indicator on the statusbar
-    //  */
-    // activityStart: function() {
-    //     PhoneGap.exec(null, null, "Notification", "activityStart", ["Busy","Please wait..."]);
-    // },
-    // 
-    // /**
-    //  * Stop spinning the activity indicator on the statusbar, if it's currently spinning
-    //  */
-    // activityStop: function() {
-    //     PhoneGap.exec(null, null, "Notification", "activityStop", []);
-    // },
-
-    // TODO are these new or old?
-    // TODO src/android/notification-android.js
-    // /**
-    //  * Display a progress dialog with progress bar that goes from 0 to 100.
-    //  *
-    //  * @param {String} title        Title of the progress dialog.
-    //  * @param {String} message      Message to display in the dialog.
-    //  */
-    // progressStart: function(title, message) {
-    //     PhoneGap.exec(null, null, "Notification", "progressStart", [title, message]);
-    // },
-    // 
-    // /**
-    //  * Set the progress dialog value.
-    //  *
-    //  * @param {Number} value         0-100
-    //  */
-    // progressValue: function(value) {
-    //     PhoneGap.exec(null, null, "Notification", "progressValue", [value]);
-    // },
-    // 
-    // /**
-    //  * Close the progress dialog.
-    //  */
-    // progressStop: function() {
-    //     PhoneGap.exec(null, null, "Notification", "progressStop", []);
-    // },
-    // 
-    // /**
-    //  * Causes the device to blink a status LED.
-    //  *
-    //  * @param {Integer} count       The number of blinks.
-    //  * @param {String} colour       The colour of the light.
-    //  */
-    // blink: function(count, colour) {
-    //     // NOT IMPLEMENTED
-    // },
 
     /**
      * Causes the device to vibrate.
@@ -116,7 +61,6 @@ navigator.notification = {
      * @param {Integer} count       The number of beeps.
      */
     beep: function(count) {
-        // TODO iOS calls new Media('beep.wav').play(); - PGNotification should implment "beep"
         PhoneGap.exec(null, null, "Notification", "beep", [count]);
     }
 };
